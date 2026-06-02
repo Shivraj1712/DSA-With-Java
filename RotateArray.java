124c10c1b1a18b78828b534c298ff58e48ee42bf
Shivraj1712 <shivrajmaharaul688@gmail.com>
2026-06-02 11:41:21 +0530

    Pushing updated RotateArray.java file

--- RotateArray.java
+++ RotateArray.java
@@ -8,20 +8,36 @@ public class RotateArray {
             right--;
         }
     }
-
-    public static void rotataArray(int[] arr, int k) {
+    public void swap(int [] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp ;
+    }
+    public static void OptRotataArray(int[] arr, int k) {
         k = k % arr.length;
         reverse(arr, 0, arr.length - 1);
         reverse(arr, 0, k - 1);
         reverse(arr, k, arr.length - 1);
     }
-
+    public static void bruteForceRotateArray(int [] arr,int k){
+        int [] ans = new int[arr.length];
+        int index = 0;
+        for(int i = k + 1 ; i < arr.length ; ++i ){
+            ans[index] = arr[i];
+            index++; 
+        }
+        for(int i = 0 ; i <= k ; ++i){
+            ans[index] = arr[i];
+            index++;
+        }
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+    }
     public static void main(String[] args) {
         int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
         int k = 3;
-        rotataArray(arr, k);
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
+        bruteForceRotateArray(arr,k);
+        
     }
 }
