4f679133b8ca5750eebdd85707f60e717cf949f3
Shivraj1712 <shivrajmaharaul688@gmail.com>
2026-06-02 12:16:48 +0530

    Pushing the file for the code of print all the subarrays

---
+++ SubarrayPrint.java
@@ -0,0 +1,17 @@
+public class SubarrayPrint{
+    public static void PrintSubarrays(int [] arr){
+        for(int i = 0 ; i < arr.length ; ++i){
+            for(int j = i ; j < arr.length ; ++j){
+                for(int k = i ; k <= j ; ++k){
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args){
+        int [] arr = new int[] {1,2,3,4,5};
+        PrintSubarrays(arr);
+    }
+}
