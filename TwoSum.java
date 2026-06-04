a4061abf916c202327d535179f0b5e9be52e59fc
Shivrajsinh <Shivrajmaharaul688@gmail.com>
2026-06-04 10:54:52 +0530

    Pushing the code till date

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+public class TwoSum {
+    public static int[] twoSum(int[] arr, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = i + 1; j < arr.length; ++j) {
+                if (arr[i] + arr[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
